6cc5ac2834053af165832e9aebb4e72475637141
sailor mars <sailorfe@proton.me>
2023-11-07 16:31:15 -0500

    layer mods

--- sailorfe/keymap.c
+++ sailorfe/keymap.c
@@ -23,6 +23,10 @@ enum planck_keycodes {
 #define CELLO   TG(_CELLO)
 #define GUITAR  TG(_GUITAR)
 
+#define TEMPCTL LM(_QWERTY, MOD_LCTL)
+#define TEMPGUI LM(_QWERTY, MOD_LGUI)
+#define TEMPALT LM(_QWERTY, MOD_LALT)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_planck_grid(
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_BSPC,
@@ -64,12 +68,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MI_C2,      MI_Cs2,     MI_D2,      MI_Ds2,     MI_E2,      MI_F2,      MI_Fs2,     MI_G2,      MI_Gs2,     MI_A2,      MI_As2,     MI_B2,
         MI_C1,      MI_Cs1,     MI_D1,      MI_Ds1,     MI_E1,      MI_F1,      MI_Fs1,     MI_G1,      MI_Gs1,     MI_A1,      MI_As1,     MI_B1,
         MI_C,       MI_Cs,      MI_D,       MI_Ds,      MI_E,       MI_F,       MI_Fs,      MI_G,       MI_Gs,      MI_A,       MI_As,      MI_B,
-        CELLO,      MI_OC2,     MI_OCTD,    MI_TRN2,    MI_TRN1,    MI_OC0,     MI_TR0,     MI_TR1,     MI_TR2,     MI_OCTU,    MI_OC4,     GUITAR     
+        CELLO,      TEMPCTL,    TEMPGUI,    TEMPALT,    MI_OCTD,    MI_OC1,     MI_OC3,     MI_OCTU,    MI_TRN2,    MI_TRN1,    MI_TR1,     MI_TR2    
     ),
     [_GUITAR] = LAYOUT_planck_grid(
         MI_E2,      MI_F2,      MI_Fs2,     MI_G2,      MI_Gs2,     MI_A2,      MI_As2,     MI_B2,      MI_C3,      MI_Cs3,     MI_D3,      MI_Ds3,
         MI_E1,      MI_F1,      MI_Fs1,     MI_G1,      MI_Gs1,     MI_A1,      MI_As1,     MI_B1,      MI_C2,      MI_Cs2,     MI_D2,      MI_Ds2,
         MI_E,       MI_F,       MI_Fs,      MI_G,       MI_Gs,      MI_A,       MI_As,      MI_B,       MI_C1,      MI_Cs1,     MI_D1,      MI_Ds1,
-        CELLO,      MI_OC2,     MI_OCTD,    MI_TRN2,    MI_TRN1,    MI_OC0,     MI_TR0,     MI_TR1,     MI_TR2,     MI_OCTU,    MI_OC4,     GUITAR
+        GUITAR,     TEMPCTL,    TEMPGUI,    TEMPALT,    MI_OCTD,    MI_OC1,     MI_OC3,     MI_OCTU,    MI_TRN2,    MI_TRN1,    MI_TR1,     MI_TR2
     )
 };
