4b088178689097b55580cf89f100920481a4ceb1
sailor mars <sailorfe@proton.me>
2023-11-07 15:50:03 -0500

    octave nav

--- sailorfe/keymap.c
+++ sailorfe/keymap.c
@@ -64,12 +64,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MI_C2,      MI_Cs2,     MI_D2,      MI_Ds2,     MI_E2,      MI_F2,      MI_Fs2,     MI_G2,      MI_Gs2,     MI_A2,      MI_As2,     MI_B2,
         MI_C1,      MI_Cs1,     MI_D1,      MI_Ds1,     MI_E1,      MI_F1,      MI_Fs1,     MI_G1,      MI_Gs1,     MI_A1,      MI_As1,     MI_B1,
         MI_C,       MI_Cs,      MI_D,       MI_Ds,      MI_E,       MI_F,       MI_Fs,      MI_G,       MI_Gs,      MI_A,       MI_As,      MI_B,
-        CELLO,      MI_OCN1,    MI_OCTD,    MI_TRN2,    MI_TRN1,    MI_OC0,     MI_TR0,     MI_TR1,     MI_TR2,     MI_OCTU,    MI_OC1,     GUITAR     
+        CELLO,      MI_OC2,     MI_OCTD,    MI_TRN2,    MI_TRN1,    MI_OC0,     MI_TR0,     MI_TR1,     MI_TR2,     MI_OCTU,    MI_OC4,     GUITAR     
     ),
     [_GUITAR] = LAYOUT_planck_grid(
         MI_E2,      MI_F2,      MI_Fs2,     MI_G2,      MI_Gs2,     MI_A2,      MI_As2,     MI_B2,      MI_C3,      MI_Cs3,     MI_D3,      MI_Ds3,
         MI_E1,      MI_F1,      MI_Fs1,     MI_G1,      MI_Gs1,     MI_A1,      MI_As1,     MI_B1,      MI_C2,      MI_Cs2,     MI_D2,      MI_Ds2,
         MI_E,       MI_F,       MI_Fs,      MI_G,       MI_Gs,      MI_A,       MI_As,      MI_B,       MI_C1,      MI_Cs1,     MI_D1,      MI_Ds1,
-        CELLO,      MI_OCN1,    MI_OCTD,    MI_TRN2,    MI_TRN1,    MI_OC0,     MI_TR0,     MI_TR1,     MI_TR2,     MI_OCTU,    MI_OC1,     GUITAR
+        CELLO,      MI_OC2,     MI_OCTD,    MI_TRN2,    MI_TRN1,    MI_OC0,     MI_TR0,     MI_TR1,     MI_TR2,     MI_OCTU,    MI_OC4,     GUITAR
     )
 };
